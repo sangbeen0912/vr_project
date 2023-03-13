4ec6556479283836666db9650f367cd73351e411
sangbeen0912 <sangbeen0912@gmail.com>
2023-03-13 12:00:56 +0900

    hello world 커밋

---
+++ vr_project/Assets/scripts/hello.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+ 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
